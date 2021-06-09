6855141619db6a1c1ade1d874368a358fe405f0b
Telmo Ricardo <telmoricardorosa@gmail.com>
2021-06-09 20:57:57 -0300

    Criando repository e service para categoria

--- src/main/java/com/telmoricardo/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/telmoricardo/cursomc/resources/CategoriaResource.java
@@ -3,15 +3,22 @@ package com.telmoricardo.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.telmoricardo.cursomc.domain.Categoria;
+import com.telmoricardo.cursomc.service.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaService categoriaService;
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public List<Categoria>  listar() {
@@ -23,6 +30,12 @@ public class CategoriaResource {
 		lista.add(cat2);
 		
 		return lista;
+	}
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = categoriaService.find(id);
+		return ResponseEntity.ok().body(obj);		
 	}
 	
 }
---
+++ src/main/java/com/telmoricardo/cursomc/service/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.telmoricardo.cursomc.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.telmoricardo.cursomc.domain.Categoria;
+import com.telmoricardo.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = categoriaRepository.findById(id);
+		return obj.orElse(null);
+	}
+}
