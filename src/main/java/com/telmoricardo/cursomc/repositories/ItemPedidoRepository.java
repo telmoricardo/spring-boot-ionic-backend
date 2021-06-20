ce8cf33e45db9edd52662398a21bf7ba71f3c64c
Telmo Ricardo <telmoricardorosa@gmail.com>
2021-06-19 23:36:27 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/telmoricardo/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,9 @@
+package com.telmoricardo.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.telmoricardo.cursomc.domain.ItemPedido;
+
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer>{
+
+}
