adfda459d892cc1498980198b495c23ae4ec90aa
Telmo Ricardo <telmoricardorosa@gmail.com>
2021-06-22 22:51:30 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/telmoricardo/cursomc/CursomcApplication.java
+++ src/main/java/com/telmoricardo/cursomc/CursomcApplication.java
@@ -70,6 +70,15 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Cama");
+		Categoria cat4 = new Categoria(null, "Mesa");
+		Categoria cat5 = new Categoria(null, "Banho");
+		Categoria cat6 = new Categoria(null, "Jardinagem");
+		Categoria cat7 = new Categoria(null, "Totem");
+		Categoria cat8 = new Categoria(null, "Camisetas");
+		Categoria cat9 = new Categoria(null, "Roupas");
+		Categoria cat10 = new Categoria(null, "Acessórios");
+		Categoria cat11 = new Categoria(null, "Eletronicos");
 		
 		Produto p1 = new Produto(null, "Computado", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -83,7 +92,7 @@ public class CursomcApplication implements CommandLineRunner{
 		p3.getCategorias().addAll(Arrays.asList(cat1));	
 		
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2,cat3, cat4, cat5, cat6,cat7,cat8,cat9,cat10,cat11));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 		Estado est1 = new Estado(null, "Minas Gerais");
--- src/main/java/com/telmoricardo/cursomc/service/CategoriaService.java
+++ src/main/java/com/telmoricardo/cursomc/service/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.telmoricardo.cursomc.domain.Categoria;
@@ -46,5 +49,11 @@ public class CategoriaService {
 
 	public List<Categoria> findAll() {		
 		return categoriaRepository.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer size, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, size, Direction.valueOf(direction), orderBy );
+		return categoriaRepository.findAll(pageRequest);
+		
 	}
 }
