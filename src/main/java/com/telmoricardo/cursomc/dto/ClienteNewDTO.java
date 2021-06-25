ae029a0e857dfca33f8cbfed2aca67b84a978bdf
Telmo Ricardo <telmoricardorosa@gmail.com>
2021-06-25 20:26:46 -0300

    Validação customizada: CPF ou CNPJ na inserção de Cliente

--- src/main/java/com/telmoricardo/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/telmoricardo/cursomc/dto/ClienteNewDTO.java
@@ -2,45 +2,56 @@ package com.telmoricardo.cursomc.dto;
 
 import java.io.Serializable;
 
-public class ClienteNewDTO implements Serializable{
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+
+
+
+public class ClienteNewDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
+
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Email(message="Email inválido")
 	private String email;
-	private String cpfouCnpj;
+
+	@NotEmpty(message="Preenchimento obrigatório")
+	private String cpfOuCnpj;
+
 	private Integer tipo;
 	
+	//@NotEmpty(message="Preenchimento obrigatório")
+	private String senha;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String logradouro;
+
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String numero;
+
 	private String complemento;
+
 	private String bairro;
+
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String cep;
 	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String telefone1;
+
 	private String telefone2;
-	private String telefone3;
 	
+	private String telefone3;
+
 	private Integer cidadeId;
-
-	public ClienteNewDTO() {}
-
-	public ClienteNewDTO(String nome, String email, String cpfouCnpj, Integer tipo, String logradouro, String numero,
-			String complemento, String bairro, String cep, String telefone1, String telefone2, String telefone3,
-			Integer cidadeId) {
-		
-		this.nome = nome;
-		this.email = email;
-		this.cpfouCnpj = cpfouCnpj;
-		this.tipo = tipo;
-		this.logradouro = logradouro;
-		this.numero = numero;
-		this.complemento = complemento;
-		this.bairro = bairro;
-		this.cep = cep;
-		this.telefone1 = telefone1;
-		this.telefone2 = telefone2;
-		this.telefone3 = telefone3;
-		this.cidadeId = cidadeId;
+	
+	public ClienteNewDTO() {
 	}
 
 	public String getNome() {
@@ -59,12 +70,12 @@ public class ClienteNewDTO implements Serializable{
 		this.email = email;
 	}
 
-	public String getCpfouCnpj() {
-		return cpfouCnpj;
+	public String getCpfOuCnpj() {
+		return cpfOuCnpj;
 	}
 
-	public void setCpfouCnpj(String cpfouCnpj) {
-		this.cpfouCnpj = cpfouCnpj;
+	public void setCpfOuCnpj(String cpfOuCnpj) {
+		this.cpfOuCnpj = cpfOuCnpj;
 	}
 
 	public Integer getTipo() {
@@ -146,8 +157,12 @@ public class ClienteNewDTO implements Serializable{
 	public void setCidadeId(Integer cidadeId) {
 		this.cidadeId = cidadeId;
 	}
-	
-	
-	
-	
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 }
---
+++ src/main/java/com/telmoricardo/cursomc/service/validation/ClienteInsertValidator.java
@@ -0,0 +1,53 @@
+package com.telmoricardo.cursomc.service.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.telmoricardo.cursomc.domain.enums.TipoCliente;
+import com.telmoricardo.cursomc.dto.ClienteNewDTO;
+import com.telmoricardo.cursomc.repositories.ClienteRepository;
+import com.telmoricardo.cursomc.resources.exception.FieldMessage;
+import com.telmoricardo.cursomc.service.validation.utils.BR;
+
+
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISISCA.getCodigo()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCodigo()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		/*Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}*/
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
