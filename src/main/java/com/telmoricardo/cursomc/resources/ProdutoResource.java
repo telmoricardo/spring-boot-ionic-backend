5fb56b55c9d9f0c821bacb1d032f8bf1949ea0d8
Telmo Ricardo <telmoricardorosa@gmail.com>
2021-06-26 11:54:00 -0300

    Busca de pedidos por nome e categorias

--- src/main/java/com/telmoricardo/cursomc/resources/ProdutoResource.java
+++ src/main/java/com/telmoricardo/cursomc/resources/ProdutoResource.java
@@ -1,17 +1,21 @@
 package com.telmoricardo.cursomc.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.telmoricardo.cursomc.domain.Categoria;
-import com.telmoricardo.cursomc.service.CategoriaService;
+import com.telmoricardo.cursomc.domain.Produto;
+import com.telmoricardo.cursomc.dto.CategoriaDTO;
+import com.telmoricardo.cursomc.dto.ProdutoDTO;
+import com.telmoricardo.cursomc.resources.utils.URL;
 import com.telmoricardo.cursomc.service.ProdutoService;
 
 @RestController
@@ -21,22 +25,26 @@ public class ProdutoResource {
 	@Autowired
 	private ProdutoService produtoService;
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria>  listar() {
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		
-		List<Categoria> lista = new ArrayList<>() ;
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
-	}
 	
 	@RequestMapping(value="/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
-		Categoria obj = produtoService.find(id);
+		Produto obj = produtoService.find(id);
 		return ResponseEntity.ok().body(obj);		
 	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<Page<ProdutoDTO>> findPage(
+			@RequestParam(value = "nome", defaultValue = "") String nome, 
+			@RequestParam(value = "categorias", defaultValue = "") String categorias, 
+			@RequestParam(value = "page", defaultValue = "0") Integer page, 
+			@RequestParam(value = "size", defaultValue = "10") Integer size, 
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy, 
+			@RequestParam(value = "direction", defaultValue = "ASC")String direction) {
+		String nomeDecode = URL.decodeParam(nome);
+		List<Integer> ids = URL.decodeIntList(categorias);
+		Page<Produto> list = produtoService.search(nomeDecode, ids, page, size, orderBy, direction);
+		Page<ProdutoDTO> listDto = list.map(obj-> new ProdutoDTO(obj));
+		return ResponseEntity.ok().body(listDto);
+	} 
 	
 }
--- src/main/java/com/telmoricardo/cursomc/service/ProdutoService.java
+++ src/main/java/com/telmoricardo/cursomc/service/ProdutoService.java
@@ -1,21 +1,37 @@
 package com.telmoricardo.cursomc.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.telmoricardo.cursomc.domain.Categoria;
+import com.telmoricardo.cursomc.domain.Produto;
 import com.telmoricardo.cursomc.repositories.CategoriaRepository;
+import com.telmoricardo.cursomc.repositories.ProdutoRepository;
 
 @Service
 public class ProdutoService {
 	
 	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
-	public Categoria find(Integer id) {
-		Optional<Categoria> obj = categoriaRepository.findById(id);
+	public Produto find(Integer id) {
+		Optional<Produto> obj = produtoRepository.findById(id);
 		return obj.orElse(null);
 	}
+	
+	public Page<Produto> search(String nome, List<Integer> ids,Integer page, Integer size, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, size, Direction.valueOf(direction), orderBy );
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return produtoRepository.search(nome, categorias, pageRequest);
+	}
+ 
 }
