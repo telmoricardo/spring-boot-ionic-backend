489dd8670577ca1af2ad832495be5ee463035e82
Telmo Ricardo <telmoricardorosa@gmail.com>
2021-07-13 21:57:19 -0300

    Salvando primerio arquivo no S3

--- src/main/java/com/telmoricardo/cursomc/CursomcApplication.java
+++ src/main/java/com/telmoricardo/cursomc/CursomcApplication.java
@@ -1,12 +1,17 @@
 package com.telmoricardo.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.telmoricardo.cursomc.service.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -15,7 +20,7 @@ public class CursomcApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
-		
+		s3Service.uploadFile("/home/telmo/Imagens/eqemp.png");
 	}
 
 }
---
+++ src/main/java/com/telmoricardo/cursomc/config/S3Config.java
@@ -0,0 +1,33 @@
+package com.telmoricardo.cursomc.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+@Configuration
+public class S3Config {
+	
+	@Value("${aws.access_key_id}")
+	private String awsId;
+	
+	@Value("${aws.secret_access_key}")
+	private String awsSecret;
+	
+	@Value("${s3.region}")
+	private String region;
+	
+	@Bean
+	public AmazonS3 s3client() {
+		
+		BasicAWSCredentials awsCred = new BasicAWSCredentials(awsId, awsSecret);
+		AmazonS3 s3client = AmazonS3ClientBuilder.standard().withRegion(region)
+				.withCredentials(new AWSStaticCredentialsProvider(awsCred)).build();
+		return s3client;
+	}
+
+}
